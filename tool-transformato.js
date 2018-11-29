219563e192d005615123fe2b0cdd3933f6d4a5a7
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2018-11-29 11:11:12 -0600

    Filequeue

--- tool-transformato.js
+++ tool-transformato.js
@@ -1,63 +1,39 @@
-const carpeta = 'Archivos/'
-const util = require('util')
-const fs = require('fs')
-const replaceExt = require('replace-ext')
-const leerCarpeta = util.promisify(fs.readdir)
-let texto = ''
+var Filequeue = require('filequeue');
+var fq = new Filequeue(1000)// max number of files to open at once
 
-var readline = require('readline');
-var stream = require('stream');
+const carpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/SQL3100/';
+const nuevaCarpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/NuevoSQL3100/'
 
-function leerArchivosXLinea (file){
-  
-  var instream = fs.createReadStream(carpeta+file, {encoding: 'utf-16le'});
-  var outstream = new stream;
-  var readStream = readline.createInterface(instream, outstream);
-  readStream.on('line', function(line){
-      texto += line + '\n'
-  })
-  readStream.on('close', () => {
-    transformar (texto, file)
-  });
- 
-}
-
-function remplazar (texto, file) {
-  console.log('Se remplazara')
-  var nuevaRuta = replaceExt(file, '.txt');
-  fs.writeFile(carpeta+'/'+nuevaRuta, texto, {encoding: 'utf-16le'},function (err) { 
-    if (err) return console.log(err);
-  }); 
-}
-
-function transformar (texto, archivo) {
-  texto = texto.replace(/\/(\*)+([^*]*(?:\*(?!)[^*]*)*(\*+)(\/))/gm,'')
-  texto = texto.replace(/\/(\*)+([^*]*(?:\*(?!)[^*]*)*(\*+)(\/))/gm,'')
-  texto = texto.replace(/\-\-+.*/gm,'')
-  texto = texto.replace(/with\(nolock\)|with \(nolock\)/mig,'')
-  texto = texto.replace(/with\(rowlock\)|with \(rowlock\)/mig,'')
-  texto = texto.replace(/((?=[\ \t])|^\s+|$)+/mg,'')
-  texto = texto.replace(/\t/mg,' ')
-  texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '') 
-  remplazar(texto, archivo)
-}
-
-/***
- * Funcion asincrona que procesa los archivos.
- * @archivos. - archivos extraidos de la ruta de la carpeta.
-***/ 
-async function procesarArchivos(archivos) {
-  for (let archivo of archivos) {
-    console.log(archivo);
-    await leerArchivosXLinea(archivo);
-  }
-}
-
-/***
- * Se ingresa la carpeta de archivos para extraerlos filtrando la terminacion .sql
- * @carpeta. - uta de la carpeta.
-***/
-leerCarpeta(carpeta).then(archivos => {
-  //procesarArchivos(archivos.filter(archivo => /.\.sql/.test(archivo)));
-  procesarArchivos(archivos);
+fq.readdir(carpeta, function(err, files) {
+    if(err) {
+        throw err
+    }
+    let counter = 0
+    files.forEach(function(file) {
+        let texto=''
+        
+        fq.readFile(carpeta + file, 'utf-16le', function(err, data) {
+            if(err) {
+                console.log('error: ', err);
+              } else {
+                texto = data
+              }
+              texto = texto.replace(/\/(\*)+([^*]*(?:\*(?!)[^*]*)*(\*+)(\/))/gm,'')
+              texto = texto.replace(/\/(\*)+([^*]*(?:\*(?!)[^*]*)*(\*+)(\/))/gm,'')
+              texto = texto.replace(/\-\-+.*/gm,'')
+              texto = texto.replace(/with\(nolock\)|with \(nolock\)/mig,'')
+              texto = texto.replace(/with\(rowlock\)|with \(rowlock\)/mig,'')
+              texto = texto.replace(/((?=[\ \t])|^\s+|$)+/mg,'')
+              texto = texto.replace(/\t/mg,' ')
+              texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '') 
+              fq.writeFile(nuevaCarpeta+file, texto, 'utf-16le', function (err) {
+                  
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!" + counter++);
+            })
+        })
+        
+    })
 })
