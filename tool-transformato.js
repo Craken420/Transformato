b038bd9e80f981cf20ba334b064023721f6e52bb
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2018-12-04 10:01:43 -0600

    Its ok

--- tool-transformato.js
+++ tool-transformato.js
@@ -1,5 +1,6 @@
-var Filequeue = require('filequeue');
-var fq = new Filequeue(1000)// max number of files to open at once
+const Filequeue = require('filequeue');
+const fq = new Filequeue(1000)// max number of files to open at once
+const chardet = require('chardet')
 
 const carpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/SQL3100/';
 const nuevaCarpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/NuevoSQL3100/'
@@ -11,27 +12,29 @@ fq.readdir(carpeta, function (err, files) {
   let counter = 0
   files.forEach(function (file) {
     let texto = ''
-    fq.readFile(carpeta + file, 'utf-16le', function(err, data) {
-      if (err) {
-        console.log('error: ', err)
-      } else {
-        texto = data
-      }
-      texto= texto.replace(/\/(\*)+(|\n+.*)([^*]*(?:\*(?!)[^*]*)*(\*+)(\/))/g, '')
-      texto= texto.replace(/\/\*([^*]*)(|[*]+|(([*]+[^*]+)*?))\*\//g, '')
-      texto = texto.replace(/(\-\-+).*/gm,'')
-      texto = texto.replace(/with\(nolock\)|with \(nolock\)/mig, '')
-      texto = texto.replace(/with\(rowlock\)|with \(rowlock\)/mig, '')
-      texto = texto.replace(/((?=[\ \t])|^\s+|$)+/mg, '')
-      texto = texto.replace(/\t/mg, ' ')
-      texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '')
-      texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '')
-      fq.writeFile(nuevaCarpeta+file, texto, 'utf-16le', function (err) {
+
+      fq.readFile(carpeta + file, 'utf-16le', function(err, data) {
         if (err) {
-          return console.log(err)
+          console.log('error: ', err)
+        } else {
+          texto = data
         }
-        console.log("The file was saved!" + counter++)
+        texto= texto.replace(/\/(\*)+(|\n+.*)([^*]*(?:\*(?!)[^*]*)*(\*+)(\/))/g, '')
+        texto= texto.replace(/\/\*([^*]*)(|[*]+|(([*]+[^*]+)*?))\*\//g, '')
+        texto = texto.replace(/(\-\-+).*/gm,'')
+        texto = texto.replace(/with\(nolock\)|with \(nolock\)/mig, '')
+        texto = texto.replace(/with\(rowlock\)|with \(rowlock\)/mig, '')
+        texto = texto.replace(/((?=[\ \t])|^\s+|$)+/mg, '')
+        texto = texto.replace(/\t/mg, ' ')
+        texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '')
+        texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '')
+        fq.writeFile(nuevaCarpeta+file, texto, 'utf-16le', function (err) {
+          if (err) {
+            return console.log(err)
+          }
+          console.log("The file was saved!" + counter++)
+        })
       })
-    })
+    
   })
 })
