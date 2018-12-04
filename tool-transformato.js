bcc0c58cdc516fc20be9229f1bd51a798fc60fec
Alma R. Bolaños Villegas <arbolanos@grupomavi.com>
2018-12-04 17:42:49 -0600

    ModificacionCarpetaNueva

--- tool-transformato.js
+++ tool-transformato.js
@@ -1,9 +1,10 @@
-var Filequeue = require('filequeue');
+//var Filequeue = require('filequeue');
 var fs= require('fs')
 //var fq = new Filequeue(1000)// max number of files to open at once
 const chardet = require('chardet')
 const path = require('path')
-const carpeta= 'Archivos'
+const carpeta= 'Archivos/'
+const nuevaCarpeta = 'ArchivosNew/'
 //const carpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/SQL3100/';
 //const nuevaCarpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/NuevoSQL3100/'
 let counter = 1
@@ -11,16 +12,13 @@ let counter = 1
 fs.readdir(carpeta, function (err, files) {
   if (err) {
     throw err
-  } files.map(function (file) {
-    return path.join(carpeta, file);
-}).filter(function (file) {
-    return fs.statSync(file).isFile();
-}).forEach(function (file) {
- 
+  } files.filter(function (file) { 
+    return fs.statSync(carpeta+file).isFile();
+}).forEach(function (files) {
     let texto = ''
     let codi=''
     let codificacion = ''
-      codificacion=chardet.detectFileSync(file)
+      codificacion=chardet.detectFileSync(carpeta+files)
       if(codificacion == 'ISO-8859-1'|codificacion == 'ISO-8859-2') {
         codi='ASCII'
       }else if(codificacion == 'UTF-8'){
@@ -30,7 +28,7 @@ fs.readdir(carpeta, function (err, files) {
       } else {
         codi = codificacion
       }
-    fs.readFile(file, codi, function(err, data) {
+    fs.readFile(carpeta+files, codi, function(err, data) {
       if (err) {
         console.log('error: ', err)
       } else {
@@ -46,14 +44,14 @@ fs.readdir(carpeta, function (err, files) {
       texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '')
       texto = texto.replace(/((?=\s(\@|\(|\=|\<|\>|\[|\]|\*|\.|\&|\,|\'|\-|\,\@|\]\(|\#|\=\@|\(\@|\/|\+|\s\w+\+|\w+)))|((?=\n)|\s)/gm, '')
     
-      fs.writeFile(file, texto, codi, function (err) {
+      fs.writeFile(nuevaCarpeta+files, texto, codi, function (err) {
         if (err) {
           return console.log(err)
         }
         console.log("The file was saved!" + counter++ + ' CON LA CODIFICACION:   ' + codificacion + ' y la CODI: '+codi+'*************')
       })
     })
-    console.log("%s", file);
-   })
+    console.log("%s", files);
+})
   
 })
