e327602e7adf50d579ce795e8153f11954b5d80a
Luis A. Peña Zuñiga <lapena@grupomavi.com>
2018-12-05 14:31:18 -0600

    RegEx comntarios

--- tool-transformato.js
+++ tool-transformato.js
@@ -3,10 +3,10 @@ const path = require('path')
 const chardet = require('chardet')
 const Filequeue = require('filequeue')
 const fq = new Filequeue(1000)   // Maximo de numero de archivos abiertos a la vez
-//const carpeta= 'Archivos/'
-//const nuevaCarpeta= 'ArchivosNEW/'
-const carpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/SQL3100/'
-const nuevaCarpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/NuevoSQL3100/'
+const carpeta= 'Archivos/'
+const nuevaCarpeta= 'ArchivosNEW/'
+//const carpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/SQL3100/'
+//const nuevaCarpeta = 'C:/Users/lapena/Documents/Luis Angel/Intelisis/NuevoSQL3100/'
 const codificaciones = ['ISO-8859-1','ISO-8859-2','ISO-8859-3','ISO-8859-4','ISO-8859-5','ISO-8859-6','ISO-8859-7','ISO-8859-8','ISO-8859-9']
 let contador = 1
 
@@ -35,8 +35,8 @@ function leerArchivo (archivo, codificacionFinal) {
 }
 
 function transformar (texto) {
-  texto = texto.replace(/\/(\*)+(|\n+.*)([^*]*(?:\*(?!)[^*]*)*(\*+)(\/))/g, '')
-  texto = texto.replace(/\/\*([^*]*)(|[*]+|(([*]+[^*]+)*?))\*\//g, '')
+  texto = texto.replace(/\/(\*+)(|\n+.*)[^*]*(?:\*((?!)[^*]*)([*]+[^*]+)*?)*?(\*+)(\/)/g, '')
+  texto = texto.replace(/\/(\*+)([^*]*)(|[*]+|(([*]+[^*]+)*?))(\*+)\//g, '')
   texto = texto.replace(/(\-\-+).*/gm, '')
   texto = texto.replace(/with(|\s+|\n+|\s+\n+)\((|\s+|\n+|\s+\n+)(rowlock|nolock)(|\s+|\n+|\s+\n+)\)/mig, '')
   texto = texto.replace(/((?=[\ \t])|^\s+|$)+/mg, '')
@@ -47,12 +47,15 @@ function transformar (texto) {
 }
 
 function remplazarTexto (archivo, texto, codificacionFinal) {
-  contador = 0
-  fq.writeFile(nuevaCarpeta+archivo.replace(/(|.*?)(\\\w+)+\\|\w+\\/g, ''), transformar(texto), codificacionFinal, function (err) {
+  let contador1 = 0
+  console.log(contador1++ + '.- CODIFICACION FINAL: ' + codificacionFinal)
+  texto = transformar(texto)
+  fq.writeFile(nuevaCarpeta+archivo.replace(/(|.*?)(\\\w+)+\\|\w+\\/g, ''), texto, {enconding : codificacionFinal}, function (err) {
     if (err) {
       return console.log(err)
     }
-    console.log(contador++ + '.- CODIFICACION: ' + codificacionFinal + '  --  ' + archivo.replace(/(|.*?)(\\\w+)+\\|\w+\\/g, ''))
+    contador1 = 0
+    console.log(contador1++ + '.- CODIFICACION ALMACENADA: ' + codificacionFinal + '  --  ' + archivo.replace(/(|.*?)(\\\w+)+\\|\w+\\/g, ''))
   })
 }
 
@@ -67,14 +70,18 @@ function codificarASCII (codificacionInicial) {
 }
 
 function detectarCodificacion (archivo) {
-  console.log('Detectando codificacion del archivo No. '+ contador++)
+  
   let codificacionInicial = ''
   codificacionInicial  = chardet.detectFileSync(archivo)
-  if (codificarASCII(codificacionInicial) == true) {
+  console.log('Detectando codificacion del archivo No. ' + contador++ + ' -- Codificacion Inicial: ' + codificacionInicial)
+  if (codificacionInicial == 'ISO-8859-1' | codificacionInicial == 'ISO-8859-2'| codificacionInicial =='ISO-8859-3'| codificacionInicial =='ISO-8859-4'| codificacionInicial =='ISO-8859-5'| codificacionInicial =='ISO-8859-6'| codificacionInicial =='ISO-8859-7'| codificacionInicial =='ISO-8859-8'| codificacionInicial =='ISO-8859-9') {
+    console.log('Retornara ASCII ')
     return 'ASCII'
   } else if (codificacionInicial == 'UTF-8') {
+    console.log('Retornara UTF-8 ')
     return 'UTF-8' 
   } else if (codificacionInicial == 'UTF-16LE' | codificacionInicial == 'UTF-32LE') {
+    console.log('Retornara UTF-16LE ')
     return 'UTF-16LE' 
   } else {
     return codificacionInicial
