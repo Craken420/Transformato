0183217476cfb550fb63617688d17ce42d9a772f
Luis Angel <luis.pena.15s@utzmg.edu.mx>
2019-04-25 16:12:31 -0500

    Refact

--- transformato.js
+++ transformato.js
@@ -1,59 +1,23 @@
+const fs = require('fs')
+const R = require('ramda')
 
-const replac = require('replace-in-file');
-/*** Sub-modulos ***/
+const { DrkBx } = require('./DarkBox/index')
 
-/* Operadores de archivos */
-const { leerCarpetaFiltrada } = require('./Utilerias/OperadoresArchivos/readDirOnlyFile')
+const pathFile = 'Testing\\dbo.AjusteAnual.StoredProcedure.sql'
 
-const regEx  = require('./Utilerias/RegEx/jsonRgx')
-const { detectarCodificacion } = require('./Utilerias/Codificacion/procesadorCodificacion')
+const proccess = R.pipe(
+    DrkBx.mix.fls.getTxtInOrgnCod,
+    DrkBx.sql.cls.ansis,
+    DrkBx.sql.cls.withNo,
+    DrkBx.sql.cls.mLineComments,
+    DrkBx.sql.cls.lineComments,
+    DrkBx.mix.cls.tab,
+    DrkBx.mix.cls.iniEndSpace,
+    DrkBx.mix.cls.onlyOneSpace,
+    DrkBx.mix.cls.emptyLinesJs
 
-const carpeta = 'Testing\\'
+)
 
-const options = {
-    files: null,
-    from: null,
-    to: '',
-    encoding: '',
-}
-
-async function replaceAsync (options) {
-    try {
-    const changes = await replac(options)
-    console.log('Archivos modificados:', regEx.Borrar.clsRuta(changes.join(', ')))
-    }
-    catch (error) {
-    console.error('Error occurrido:', error);
-    }
-}
-
-function limpiar (archivo, codificacion) {
-    options.files = archivo
-    options.encoding = codificacion
-    options.from = [
-       regEx.Expresiones.comentarioSQLVacio,
-       regEx.Expresiones.comentarioSQLSencillo, 
-       regEx.Crear.comentarioSQLMedio(), 
-       regEx.Expresiones.comentarioSQLAvanzado, 
-       regEx.Expresiones.comentarioSQLDobleGuion,
-       regEx.Crear.ansis(),
-       regEx.Crear.witchNolock(),
-       regEx.Expresiones.saltoLineaVacio,
-       regEx.Expresiones.ampersand
-       //regEx.Crear.espaciosEntrePalabras(),
-   ]
-   options.to = ''
-
-   replaceAsync(options)
-
-   options.files = archivo
-   options.from = regEx.Expresiones.tabulador,
-   options.to = ' '
-   replaceAsync(options)
-}
-
-leerCarpetaFiltrada(carpeta, ['.sql','.vis','.frm','.esp','.tbl','.rep','.dlg'])
-    .then(archivos => {
-        archivos.forEach(archivo => limpiar(archivo, (detectarCodificacion(archivo) == 'ISO-8859-1')  ? 'ascii' : detectarCodificacion(archivo)))
-    })
-
+fs.writeFileSync('Data\\' + DrkBx.mix.cls.pthRoot(pathFile), proccess(pathFile), 'utf16le')
+// 
+console.log(proccess(pathFile))
